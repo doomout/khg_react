34bef14efaf81c6f56dd73bad761e402445fc98b
khg <winquake3@hanmail.net>
2025-05-11 22:10:58 +0900

    간단한 HTML 코드 작성

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello React!
+    <div>
+      <header>
+        <h1><a href="/">WEB</a></h1>
+      </header>
+      <nav>
+        <ol>
+          <li><a href="/read/1">HTML</a></li>
+          <li><a href="/read/2">CSS</a></li>
+          <li><a href="/read/3">JavaScript</a></li>
+        </ol>
+      </nav>
+      <article>
+        <h2>Welcome</h2>
+        Hello, WEB
+      </article>
     </div>
   );
 }
