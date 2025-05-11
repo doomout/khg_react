451df9635888af74f4693c98a2d5bdd148c71a77
khg <winquake3@hanmail.net>
2025-05-11 22:18:50 +0900

    사용자 정의 태그 예제

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+// 사용자 정의 태그(반드시 대문자로 시작해야 한다.)
+function Header() {
+  return (
+    <header>
+      <h1><a href="/">React</a></h1>
+    </header>
+  )
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <h1><a href="/">WEB</a></h1>
-      </header>
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
       <nav>
         <ol>
           <li><a href="/read/1">HTML</a></li>
