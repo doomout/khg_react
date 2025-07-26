a0c9fcdebe7c953267fa0f76b6531ddaf4c4ebfe
khg <winquake3@hanmail.net>
2025-07-27 02:50:58 +0900

    Nav, Article 컴포넌트로 변환

--- src/App.js
+++ src/App.js
@@ -8,23 +8,32 @@ function Header() {
   )
 }
 
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li><a href="/read/1">html</a></li>
+        <li><a href="/read/2">css</a></li>
+        <li><a href="/read/3">js</a></li>
+      </ol>
+    </nav>
+  )
+}
+
+function Article() {
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Hello, WEB
+    </article>
+  )
+}
 function App() {
   return (
     <div>
       <Header></Header>
-      <Header></Header>
-      <Header></Header>
-      <nav>
-        <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
-          <li><a href="/read/3">js</a></li>
-        </ol>
-      </nav>
-      <article>
-        <h2>Welcome</h2>
-        Hello, WEB
-      </article>
+      <Nav></Nav>
+      <Article></Article>
     </div>
   );
 }
