5892853efa52f2ed0255597be317b3af034dc8b9
khg <winquake3@hanmail.net>
2025-07-27 02:48:08 +0900

    첫 컴포넌트 제작

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+function Header() {
+  return (
+    <header>
+      <h1><a href="/">React</a></h1>
+    </header>
+  )
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <h1><a href="/">WEB</a></h1>
-      </header>
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
       <nav>
         <ol>
           <li><a href="/read/1">html</a></li>
