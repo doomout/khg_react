5bd7124107a4039582450f30d0e750015cb88aee
khg <winquake3@hanmail.net>
2025-07-29 04:52:28 +0900

    선택한 id와 일치하는 원소를 찾아서 제목과 본문으로 설정

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ function Nav(props) {
     lis.push(<li key={t.id}>
       <a id={t.id} href={'/read/'+t.id} onClick={event=>{
         event.preventDefault();
-        props.onChangMode(event.target.id);
+        // 태그의 속성에서 오는 id는 문자 이기에 형변환으로 숫자로 변환
+        props.onChangMode(Number(event.target.id));
       }}>{t.title}</a>
     </li>);
   }
@@ -45,7 +46,7 @@ function Article(props) {
 function App() {
   // [인덱스의 값을 읽기, state의 값을 변경하기]
   const [mode, setMode] = useState('WELCOME');
-  console.log('mode', mode);
+  const [id, setId] = useState(null);
   // 네비게이션의 목옥을 담을 topice 배열 변수(제목, 본문, 각자의 ID 값)
   const topics = [
     {id:1, title:'html', body:'html is ...'},
@@ -56,15 +57,24 @@ function App() {
   if(mode === 'WELCOME') {
     content = <Article title="Welcome" body="Hello, WEB"></Article>
   } else if(mode === 'READ') {
-    content = <Article title="Read" body="Hello, Read"></Article>
+    let title, body = null;
+    for(let i=0; i<topics.length; i++) {
+      console.log(topics[i].id, id);
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
   }
   return (
     <div>
       <Header title="WEB" onChangMode={()=>{
         setMode('WELCOME'); // mode의 값을 바꿀 때는 setMode를 사용해야 한다
       }}></Header>
-      <Nav topics={topics} onChangMode={(id)=>{
-        setMode('READ'); // mode의 값을 바꿀 때는 setMode를 사용해야 한다
+      <Nav topics={topics} onChangMode={(_id)=>{
+        setMode('READ'); 
+        setId(_id);
       }}></Nav>
       {content} 
     </div>
