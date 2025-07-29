20d5703da37a3c3037bb36f7713f59dc38cd9cde
khg <winquake3@hanmail.net>
2025-07-29 22:25:17 +0900

    설정 초기화

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { defineConfig } from "eslint/config"; 
 
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat.recommended, 
 ]);
