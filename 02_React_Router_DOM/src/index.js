3e1a7d015a7c497ff96ef176b1617581caf81ab5
khg <winquake3@hanmail.net>
2025-08-01 23:14:10 +0900

    React Router DOM 기초 코드 셋팅

---
+++ 02_React_Router_DOM/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+function App() {
+  return (
+    <div>Hello React Router DOM</div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+
+reportWebVitals();
