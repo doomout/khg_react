853576f916933579faced296b3eff700c792ca8c
khg <winquake3@hanmail.net>
2025-08-04 02:40:55 +0900

    styled 버튼 만들고 속성, 상속까지

--- 03_styled_components/src/App.js
+++ 03_styled_components/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const SimpleButton = styled.button`
+  color: white;
+  background-color: green;
+`;
+
+const LargeButton = styled(SimpleButton)`
+  font-size: 50px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <SimpleButton>Simple</SimpleButton>
+      <LargeButton>Large</LargeButton>
+    </div>    
   );
 }
 
