75860bd238f0178993349f20312abb4fed9079cf
khg <doomout1@gmail.com>
2025-08-05 02:15:19 +0900

    사용자 정의 테마 만들고 한번에 적용

--- 04_context_api/src/App.js
+++ 04_context_api/src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
+import React, { createContext, useContext } from 'react';
 import './App.css';
+import "./style.css";
+const themeDefault = { border: '10px solid red' }
+const themeContext = createContext(themeDefault);
+
 
 function App() {
+  const theme = useContext(themeContext);
+  console.log('aaa', theme);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="root" style={theme}>
+      <h1>Hello World!</h1>
+      <Sub1 />
     </div>
   );
 }
 
+function Sub1() {
+  const theme = useContext(themeContext);
+  return (
+    <div style={theme}>
+      <h1>Sub1</h1>
+      <Sub2 />
+    </div>
+  );
+}
+function Sub2() {
+  const theme = useContext(themeContext);
+  return (
+    <div style={theme}>
+      <h1>Sub2</h1>
+      <Sub3 />
+    </div>
+  );
+}
+function Sub3() {
+  const theme = useContext(themeContext);
+  return (
+    <div style={theme}>
+      <h1>Sub3</h1>
+    </div>
+  );
+}
 export default App;
