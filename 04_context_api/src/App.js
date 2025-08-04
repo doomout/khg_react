3e17e2f7b6014e0c0ab553393ef248a5f95ad28f
khg <doomout1@gmail.com>
2025-08-05 02:29:11 +0900

    컨텍스트 적용

--- 04_context_api/src/App.js
+++ 04_context_api/src/App.js
@@ -8,22 +8,25 @@ const themeContext = createContext(themeDefault);
 
 function App() {
   const theme = useContext(themeContext);
-  console.log('aaa', theme);
   return (
-    <div className="root" style={theme}>
-      <h1>Hello World!</h1>
-      <Sub1 />
-    </div>
+    <themeContext.Provider value={{border: '10px solid blue'}}>
+      <div className="root" style={theme}>
+        <h1>Hello World!</h1>
+        <Sub1 />
+      </div>
+    </themeContext.Provider>
   );
 }
 
 function Sub1() {
   const theme = useContext(themeContext);
   return (
-    <div style={theme}>
-      <h1>Sub1</h1>
-      <Sub2 />
-    </div>
+    <themeContext.Provider value={{border: '10px solid green'}}>
+      <div style={theme}>
+        <h1>Sub1</h1>
+        <Sub2 />
+      </div>
+    </themeContext.Provider>
   );
 }
 function Sub2() {
