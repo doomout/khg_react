579f5aa5fb994015f937d76e08f3aa48ea51730a
khg <winquake3@hanmail.net>
2025-08-06 21:19:06 +0900

    useState 함수 만들고 클릭 이벤트까지 연결

--- 05_usereducer/src/App.js
+++ 05_usereducer/src/App.js
@@ -1,12 +1,23 @@
 import logo from './logo.svg';
+import React, {useState} from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  function down() {
+    setCount(count - 1);
+  }
+  function reset() {
+    setCount(0);
+  }
+  function up() {
+    setCount(count + 1);
+  }
   return (
     <div>
-      <input type='button' value="-" />
-      <input type='button' value="0" />
-      <input type='button' value="+" />
-      <span>0</span>
+      <input type='button' value="-" onClick={down} />
+      <input type='button' value="0" onClick={reset} />
+      <input type='button' value="+" onClick={up} />
+      <p><span>{count}</span></p>
     </div>
   );
 }
