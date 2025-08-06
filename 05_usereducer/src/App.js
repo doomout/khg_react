671f34e410b21f9bccd7a8329d2601a3743e4c02
khg <winquake3@hanmail.net>
2025-08-06 21:01:23 +0900

    useReducer 기초코드

---
+++ 05_usereducer/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div>
+      <input type='button' value="-" />
+      <input type='button' value="0" />
+      <input type='button' value="+" />
+      <span>0</span>
+    </div>
+  );
+}
+
+export default App;
