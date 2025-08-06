b0089e597b0624f7cff995e62b2c5bbc6b6de5da
khg <winquake3@hanmail.net>
2025-08-07 01:11:33 +0900

    리듀스 함수를 사용하여 객체 전달

--- 05_usereducer/src/App.js
+++ 05_usereducer/src/App.js
@@ -1,31 +1,37 @@
 import logo from './logo.svg';
-import React, {useReducer} from 'react';
+import React, {useReducer, useState} from 'react';
 
 function App() {
+  const [number, setNumber] = useState(1);
   function countReducer(oldCount, action) {
-    if(action === 'UP') {
-      return oldCount + 1;
-    } else if(action === 'DOWN') {
-      return oldCount - 1;
-    } else if(action === 'RESET') {
+    if(action.type === 'UP') {
+      return oldCount + action.number;
+    } else if(action.type === 'DOWN') {
+      return oldCount - action.number;
+    } else if(action.type === 'RESET') {
       return 0;
     }
   }
   const [count, countDispatch] = useReducer(countReducer, 0);
   function down() {
-    countDispatch('DOWN');
+    countDispatch({type: 'DOWN', number: number});
   }
   function reset() {
-    countDispatch('RESET');
+    countDispatch({type: 'RESET', number: number});
   }
   function up() {
-    countDispatch('UP');
+    countDispatch({type: 'UP', number: number});
+  }
+
+  function changNumber(event) {
+    setNumber(Number(event.target.value));
   }
   return (
     <div>
-      <input type='button' value="-" onClick={down} />
-      <input type='button' value="0" onClick={reset} />
-      <input type='button' value="+" onClick={up} />
+      <input type="button" value="-" onClick={down} />
+      <input type="button" value="0" onClick={reset} />
+      <input type="button" value="+" onClick={up} />
+      <p><input type="text" value={number} onChange={changNumber} /></p>
       <p><span>{count}</span></p>
     </div>
   );
