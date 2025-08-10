b79445fb2bb257e1f9a35441bb292a224f7f36ad
khg <winquake3@hanmail.net>
2025-08-10 18:01:31 +0900

    전체적인 설명 추가

--- 06_react_redux/src/App.js
+++ 06_react_redux/src/App.js
@@ -1,62 +1,81 @@
 import './App.css';
+// Redux에서 스토어 생성 함수 불러오기
 import { createStore } from 'redux';
-import { Provider, useSelector, useDispatch, connect } from 'react-redux';
+// React-Redux에서 스토어를 React 컴포넌트에 연결하기 위한 도구들 불러오기
+import { Provider, useSelector, useDispatch } from 'react-redux';
 
-// currentState: 현재의 상태 값, action: 현재의 값을 어떻게 바꿀 것인지 결정하는 요청
+// -------------------------
+// 리듀서 함수 정의
+// -------------------------
+// currentState: 현재 상태 값
+// action: 상태를 어떻게 바꿀지에 대한 '요청 객체'
 function reducer(currentState, action) {
-  // 스테이트가 정의되지 않았으면 기본값 1을 지정
-  if(currentState === undefined) 
-  {
+  // 스토어가 처음 만들어질 때 상태값이 없으므로 초기 상태를 지정
+  if (currentState === undefined) {
     return {
-      number: 1,
+      number: 1, // 초기값
     };
   }
-  const newState = {...currentState}; // 복사
-  if(action.type === 'PLUS') {
+
+  // 기존 상태를 복사 (불변성 유지)
+  const newState = { ...currentState };
+
+  // action의 type이 'PLUS'면 number 값을 1 증가
+  if (action.type === 'PLUS') {
     newState.number++;
   }
+
+  // 변경된 상태 반환
   return newState;
 }
+
+// -------------------------
+// 스토어 생성
+// -------------------------
 const store = createStore(reducer);
 
+// -------------------------
+// App 컴포넌트
+// -------------------------
 function App() {
   return (
     <div id="container">
       <h1>Root</h1>
       <div id="grid">
+        {/* Provider로 감싸서 store를 하위 컴포넌트에서 사용 가능하게 함 */}
         <Provider store={store}>
-          <Left1></Left1>
-          <Right1></Right1>
+          <Left1 />
+          <Right1 />
         </Provider>
       </div>
     </div>
   );
 }
 
-function Left1(props) {
+// -------------------------
+// 왼쪽 영역 컴포넌트
+// -------------------------
+function Left1() {
   return (
     <div>
       <h1>Left1</h1>
-      <Left2></Left2>
+      <Left2 />
     </div>
   );
 }
 
-function Left2(props) {
+function Left2() {
   return (
     <div>
       <h1>Left2</h1>
-      <Left3></Left3>
+      <Left3 />
     </div>
   );
 }
 
-
-function Left3(props) {
-  function f(state) {
-    return state.number;
-  }
-  const number = useSelector((state)=>state.number);
+function Left3() {
+  // store에 있는 number 값을 꺼내오기
+  const number = useSelector((state) => state.number);
   return (
     <div>
       <h1>Left3: {number}</h1>
@@ -64,35 +83,43 @@ function Left3(props) {
   );
 }
 
-function Right1(props) {
+// -------------------------
+// 오른쪽 영역 컴포넌트
+// -------------------------
+function Right1() {
   return (
     <div>
       <h1>Right1</h1>
-      <Right2></Right2>
+      <Right2 />
     </div>
   );
 }
 
-function Right2(props) {
+function Right2() {
   return (
     <div>
       <h1>Right2</h1>
-      <Right3></Right3>
+      <Right3 />
     </div>
   );
 }
 
-function Right3(props) {
+function Right3() {
+  // dispatch 함수를 가져와서 액션을 발생시킬 수 있게 함
   const dispatch = useDispatch();
   return (
     <div>
       <h1>Right3</h1>
-      <input type="button" value="+" onClick={()=>{
-        dispatch({type: 'PLUS'});
-      }} />
+      {/* 버튼 클릭 시 'PLUS' 액션을 디스패치해서 상태 변경 */}
+      <input
+        type="button"
+        value="+"
+        onClick={() => {
+          dispatch({ type: 'PLUS' });
+        }}
+      />
     </div>
   );
 }
-
 
 export default App;
