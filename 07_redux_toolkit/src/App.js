630a44fc5021aae9a5d7f088ce717709492c78b6
khg <winquake3@hanmail.net>
2025-08-11 22:01:17 +0900

    카운터가 스토어의 value 값을 가져올 수 있도록 useSelector 사용

--- 07_redux_toolkit/src/App.js
+++ 07_redux_toolkit/src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import './App.css';
+import {createStore} from 'redux'
+import {Provider, useSelector} from 'react-redux';
+
+// 리듀서 정의
+function reducer(state, action) {
+  return state;
+}
+createStore(reducer);
+
+//초기값 만들고 0으로 설정
+const initialState = {value:0}
+const state = createStore(reducer, initialState);
 
 function Counter() {
+  const count = useSelector(state => state.value);
   return (
     <div>
-      <button>+</button> 0
+      <button>+</button> {count}
     </div>
   );
 }
 function App() {
   return (
-    <div>
-      <Counter></Counter>
-    </div>
+    <Provider store={state}>
+      <div>
+        <Counter></Counter>
+      </div>
+    </Provider>
   );
 }
 
