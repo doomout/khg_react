431adbe1bcd4c4049b3a4399014d1aa99ba67a63
khg <winquake3@hanmail.net>
2025-08-12 16:53:06 +0900

    슬라이스 생성하고 스토어에 등록까지

--- 07_redux_toolkit/src/App.js
+++ 07_redux_toolkit/src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './App.css';
-import {createStore} from 'redux'
 import {Provider, useSelector, useDispatch} from 'react-redux';
+import {createSlice, configureStore} from '@reduxjs/toolkit';
 
+//슬라이스 정의
+const counterSlice = createSlice({
+  name:'counterSlice', // 슬라이스 이름
+  initialState:{value:0}, // 초기값
+  reducers:{// 리듀서(복수형)
+    up:(state, action)=>{
+      state.value = state.value + action.stop;
+    }
+  } 
+});
+
+// 여러개의 슬라이스를 모아서 스토어를 만들 때 
+const store = configureStore({
+  reducer:{ //리듀서(단수형)
+    counter:counterSlice.reducer
+  }
+});
+
+/* 슬라이스로 대체 함
 // 리듀서 정의
 function reducer(state, action) {
   // up 이라는 타입의 클릭 이벤트가 발생하면.
@@ -12,15 +31,18 @@ function reducer(state, action) {
   }
   return state;
 }
-createStore(reducer);
 
 //초기값 만들고 0으로 설정
 const initialState = {value:0}
 const state = createStore(reducer, initialState);
+*/
 
 function Counter() {
   const dispatch = useDispatch();
-  const count = useSelector(state => state.value);
+  const count = useSelector((state) => {
+    console.log(state);
+    return state.counter.value;
+  });
   return (
     <div>
       <button onClick={()=>{ //버튼 클릭시 숫자가 2씩 증가하도록 dispatch 설정
@@ -31,7 +53,7 @@ function Counter() {
 }
 function App() {
   return (
-    <Provider store={state}>
+    <Provider store={store}>
       <div>
         <Counter></Counter>
       </div>
