0d6cb5d669195b2cfa2efb4d2052efe5b11ba093
khg <winquake3@hanmail.net>
2025-08-14 16:44:35 +0900

    Router 사용하여 id 값을 가져오기

--- 08_next_js/src/pages/sub/[id].js
+++ 08_next_js/src/pages/sub/[id].js
@@ -1,8 +1,13 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Id() {
+    const router = useRouter();
+    // 라우터를 사용하여 id를 가져오고 문자열 -> 숫자로 변환 후 id에 저장
+    const id = Number(router.query.id);
     return <>
         <h1>/pages/sub/[id].js</h1>
+        <p>Parameter id: {id}</p>
         <Link href="/">/pages/index.js</Link>
     </>
 }
