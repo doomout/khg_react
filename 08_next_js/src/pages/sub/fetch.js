6786a4280933a53c898c4c895f780cfbc453e190
khg <winquake3@hanmail.net>
2025-08-14 17:22:18 +0900

    무한으로 Fast Refresh 뜨는 에러 수정

--- 08_next_js/src/pages/sub/fetch.js
+++ 08_next_js/src/pages/sub/fetch.js
@@ -6,11 +6,9 @@ export default function Fetch() {
     useEffect(() => {
         fetch(process.env.NEXT_PUBLIC_API_URL+'api/hello')
         .then(type=>type.json())
-        .then(result=>{
-            //console.log(result);
-            setUser(result);
-        })
-    });
+        .then(result=>{setUser(result);})
+        .catch(err => console.error(err)); //api 호출시 에러 확인용
+    }, []); //빈 배열 추가하여 처음 렌더링 될 때 한번만 실행 되도록
     return <>
         <h1>/pages/sub/fetch.js</h1>
         <p>name: {user.name}</p>
